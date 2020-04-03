a79e2cfbe8719959348e4898e19c4d3354061418
Moe Green <gearmobile@gmail.com>
2020-04-03 18:40:49 +0300

    fix: fix some errors

--- src/js/Game.js
+++ src/js/Game.js
@@ -6,10 +6,10 @@ export default class Game {
   }
 
   start() {
-    const gameField = document.querySelector(".game-field");
+    const gameField = document.querySelector('.game-field');
     for (let i = 0; i < this.gameFieldSize; i += 1) {
       const newCell = `<div class='cell' id=cell${i}></div>`;
-      gameField.insertAdjacentHTML("beforeend", newCell);
+      gameField.insertAdjacentHTML('beforeend', newCell);
     }
     this.showGoblin();
   }
@@ -18,12 +18,10 @@ export default class Game {
     setInterval(() => {
       this.setRandomIdx();
 
-      const currentCell = document.getElementById(
-        `cell${this.currentCellIndex}`
-      );
+      const currentCell = document.getElementById(`cell${this.currentCellIndex}`);
       const nextCell = document.getElementById(`cell${this.nextCellIndex}`);
 
-      currentCell.innerHTML = "";
+      currentCell.innerHTML = '';
       nextCell.appendChild(this.generateNewImage());
       this.currentCellIndex = this.nextCellIndex;
     }, 1000);
@@ -35,10 +33,11 @@ export default class Game {
     } while (this.currentCellIndex === this.nextCellIndex);
   }
 
+  // eslint-disable-next-line class-methods-use-this
   generateNewImage() {
     const image = new Image();
-    image.src = "src/img/goblin.png";
-    image.classList.add("cell-image");
+    image.src = 'src/img/goblin.png';
+    image.classList.add('cell-image');
     return image;
   }
 }
